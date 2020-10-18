0d2cc72326e8f4b6860f13cfe225be18e2a49fd1
jmolly <roshkovskypetr@gmail.com>
2020-10-18 15:44:07 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,59 @@
-module.exports = function toReadable (number) {
-  
-}
+const decimal = [
+    "",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+];
+const tens = [
+    "",
+    "",
+    "twenty",
+    "thirty",
+    "forty",
+    "fifty",
+    "sixty",
+    "seventy",
+    "eighty",
+    "ninety",
+];
+const dozens = [
+    "ten",
+    "eleven",
+    "twelve",
+    "thirteen",
+    "fourteen",
+    "fifteen",
+    "sixteen",
+    "seventeen",
+    "eighteen",
+    "nineteen",
+];
+
+const toReadableTens = (num) => {
+    if (num < 10) return decimal[num];
+    else if (num >= 10 && num < 20) return dozens[num - 10];
+    else {
+        return tens[Math.floor(num / 10)] + " " + decimal[num % 10];
+    }
+};
+
+module.exports = function toReadable(number) {
+    let result = "";
+
+    if (number >= 100) {
+        result =
+            decimal[Math.floor(number / 100)] +
+            " hundred " +
+            toReadableTens(number % 100);
+    } else {
+        result = toReadableTens(number);
+    }
+
+    return number === 0 ? "zero" : result.trim();
+};
